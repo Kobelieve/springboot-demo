9583b567c04d806cfd47d732d1988df64ea41000
hutao <15622719535@163.com>
2019-03-23 18:24:50 +0800

    启动器

--- src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动器
+ */
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
